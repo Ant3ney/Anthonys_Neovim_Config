3af31ff5429a972510fce64408007aeeb16c9809
Anthony Cavuoti <anthonycavuoti@gmail.com>
2026-01-12 22:02:56 -0800

    Try more practice

--- lua/Module_Practice/p.bat
+++
@@ -1,9 +0,0 @@
-setlocal
-
-set project_path=____@@@@project_path@@@@____
-set project_name=____@@@@project_name@@@@____
-set ubt_path=____@@@@ubt_path@@@@____
-
-powershell -noprofile -command "dotnet \"\"\"%ubt_path%\"\"\" -project="%project_path%" "%project_name%Editor" development win64 -waitmutex -frommsbuild -arcitecture=x64
-
-endlocal
